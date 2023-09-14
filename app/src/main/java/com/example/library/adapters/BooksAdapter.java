c4e6fc809bc35ab1f0f419ecd69ed51a41cf7bba
Misbah <142839155+misbahSyedd@users.noreply.github.com>
2023-09-14 14:44:33 -0500

    Commnets added to BooksAdapter.java

--- app/src/main/java/com/example/library/adapters/BooksAdapter.java
+++ app/src/main/java/com/example/library/adapters/BooksAdapter.java
@@ -3,8 +3,6 @@ package com.example.library.adapters;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,11 +25,9 @@ import static com.example.library.activities.AppHome.CATEGORY;
 import static com.example.library.activities.AppHome.COVER_URL;
 import static com.example.library.activities.AppHome.DESCRIPTIONS;
 import static com.example.library.activities.AppHome.ISBN;
-//import static com.example.library.activities.AppHome.LOGGED;
 import static com.example.library.activities.AppHome.REQUESTS;
 import static com.example.library.activities.AppHome.TITLE;
 import static com.example.library.activities.AppHome.UPLOADED_BY;
-
 
 public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHolder> {
 
@@ -39,18 +35,18 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
     String mlogged, mrole;
     private ArrayList<Books> marrayList;
 
-
-    public BooksAdapter(Context context, ArrayList<Books> arrayList, String logged, String role){
+    // Constructor for the adapter
+    public BooksAdapter(Context context, ArrayList<Books> arrayList, String logged, String role) {
         marrayList = arrayList;
         mcontext = context;
         mlogged = logged;
         mrole = role;
-
     }
 
     @NonNull
     @Override
     public BooksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the item view for the RecyclerView
         View view = LayoutInflater.from(mcontext).inflate(R.layout.book_card, parent, false);
         return new BooksViewHolder(view);
     }
@@ -59,6 +55,7 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
     public void onBindViewHolder(@NonNull BooksViewHolder holder, int position) {
         Books currentBook = marrayList.get(position);
 
+        // Extract book information from the current item
         final String title = currentBook.getTitle();
         final String author = currentBook.getAuthor();
         final String cover = currentBook.getCover();
@@ -69,6 +66,7 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
         final String description = currentBook.getDescript();
         final String requested = currentBook.getRequests();
 
+        // Set the extracted information to the corresponding views
         holder.bookTitle.setText(title);
         holder.bookAuthor.setText(author);
         holder.bookDescriptions.setText(description);
@@ -77,34 +75,39 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
         holder.uploader.setText(uploadedby);
         holder.available.setText(available);
         holder.Requests.setText(requested);
-        if(cover.equals("")){
+
+        // Load book cover image using Picasso library
+        if (cover.equals("")) {
             holder.bookCover.setImageResource(R.drawable.book_cover);
-        }else{
+        } else {
             Picasso.get().load(cover).fit().centerInside().into(holder.bookCover);
         }
 
+        // Handle click events on the item view
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Create an intent to open the BookDetails activity
                 Intent intent = new Intent(mcontext, BookDetails.class);
+
+                // Pass book information as extras to the intent
                 intent.putExtra(COVER_URL, cover);
                 intent.putExtra(TITLE, title);
                 intent.putExtra(AUTHOR, author);
-                intent.putExtra("Mail",mlogged);
-                intent.putExtra("ROLE",mrole);
+                intent.putExtra("Mail", mlogged);
+                intent.putExtra("ROLE", mrole);
                 intent.putExtra(ISBN, isbn);
                 intent.putExtra(UPLOADED_BY, uploadedby);
                 intent.putExtra(AVAILABLE, available);
                 intent.putExtra(CATEGORY, category);
                 intent.putExtra(DESCRIPTIONS, description);
                 intent.putExtra(REQUESTS, requested);
+
+                // Start the BookDetails activity and finish the current activity
                 mcontext.startActivity(intent);
-                ((Activity)mcontext).finish();
+                ((Activity) mcontext).finish();
             }
         });
-
-
     }
 
     @Override
@@ -112,10 +115,13 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
         return marrayList.size();
     }
 
+    // ViewHolder class to hold and manage the item view's UI elements
     public class BooksViewHolder extends RecyclerView.ViewHolder {
         public ImageView bookCover;
         View view;
         public TextView bookTitle, bookAuthor, bookDescriptions, available, isbn, uploader, bookCategory, Requests;
+
+        // Constructor for the ViewHolder
         public BooksViewHolder(@NonNull View itemView) {
             super(itemView);
             bookAuthor = itemView.findViewById(R.id.author);
@@ -128,14 +134,12 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHol
             uploader = itemView.findViewById(R.id.uploadby);
             bookCategory = itemView.findViewById(R.id.category);
             Requests = itemView.findViewById(R.id.requs);
-
         }
     }
 
+    // Method to filter the list of books
     public void filterList(ArrayList<Books> filteredList) {
         marrayList = filteredList;
         notifyDataSetChanged();
     }
-
-
 }
